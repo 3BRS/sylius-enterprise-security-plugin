803f575486cd42be5a9257b6f2b63ebaf4526e97
Filip <filiphoracek472@gmail.com>
2026-04-09 12:48:05 +0200

    fixup! fixup! fixup! fixup! fixup! SLS-101: Password policy

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace ThreeBRS\SyliusEnterpriseSecurityPlugin\DependencyInjection;
 
-use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
@@ -16,7 +15,6 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('three_brs_sylius_enterprise_security');
-        /** @var ArrayNodeDefinition $rootNode */
         $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
