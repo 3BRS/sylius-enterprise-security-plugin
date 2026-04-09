01f7f883cca3ea34160a86570a023de088ba605f
Filip <filiphoracek472@gmail.com>
2026-04-09 16:43:05 +0200

    fixup! fixup! fixup! fixup! SLS-100 monorepo, CL, test app

--- src/ThreeBRSSyliusEnterpriseSecurityPlugin.php
+++ src/ThreeBRSSyliusEnterpriseSecurityPlugin.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace ThreeBRS\SyliusEnterpriseSecurityPlugin;
 
+use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class ThreeBRSSyliusEnterpriseSecurityPlugin extends Bundle
 {
+    use SyliusPluginTrait;
 }
