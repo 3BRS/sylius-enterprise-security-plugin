5bfcd8761ee383d47706eeeeb6d036e66c80e793
Filip <filiphoracek472@gmail.com>
2026-04-10 11:04:48 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! SLS-101: Password policy

---
+++ src/Validator/PasswordPolicyFilteringValidator.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ThreeBRS\SyliusEnterpriseSecurityPlugin\Validator;
+
+use Symfony\Component\Validator\ConstraintViolationList;
+use Symfony\Component\Validator\ConstraintViolationListInterface;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
+use Symfony\Component\Validator\Mapping\MetadataInterface;
+use Symfony\Component\Validator\Validator\ContextualValidatorInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class PasswordPolicyFilteringValidator implements PasswordPolicyFilteringValidatorInterface
+{
+    public function __construct(private ValidatorInterface $inner)
+    {
+    }
+
+    public function validate(mixed $value, mixed $constraints = null, mixed $groups = null): ConstraintViolationListInterface
+    {
+        return $this->filter($this->inner->validate($value, $constraints, $groups));
+    }
+
+    public function validateProperty(object $object, string $propertyName, mixed $groups = null): ConstraintViolationListInterface
+    {
+        return $this->filter($this->inner->validateProperty($object, $propertyName, $groups));
+    }
+
+    public function validatePropertyValue(object|string $objectOrClass, string $propertyName, mixed $value, mixed $groups = null): ConstraintViolationListInterface
+    {
+        return $this->filter($this->inner->validatePropertyValue($objectOrClass, $propertyName, $value, $groups));
+    }
+
+    public function startContext(): ContextualValidatorInterface
+    {
+        return $this->inner->startContext();
+    }
+
+    public function inContext(ExecutionContextInterface $context): ContextualValidatorInterface
+    {
+        return $this->inner->inContext($context);
+    }
+
+    public function getMetadataFor(mixed $value): MetadataInterface
+    {
+        return $this->inner->getMetadataFor($value);
+    }
+
+    public function hasMetadataFor(mixed $value): bool
+    {
+        return $this->inner->hasMetadataFor($value);
+    }
+
+    private function filter(ConstraintViolationListInterface $violations): ConstraintViolationListInterface
+    {
+        $passwordPolicyPaths = [];
+        foreach ($violations as $violation) {
+            if (str_starts_with($violation->getMessageTemplate(), 'three_brs.password_policy.')) {
+                $passwordPolicyPaths[$violation->getPropertyPath()] = true;
+            }
+        }
+
+        if ($passwordPolicyPaths === []) {
+            return $violations;
+        }
+
+        $filtered = new ConstraintViolationList();
+        foreach ($violations as $violation) {
+            if (
+                $violation->getMessageTemplate() === 'sylius.user.password.min' &&
+                isset($passwordPolicyPaths[$violation->getPropertyPath()])
+            ) {
+                continue;
+            }
+
+            $filtered->add($violation);
+        }
+
+        return $filtered;
+    }
+}
